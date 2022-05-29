de996574851d378e021f5c012263179e77e11ef4
jiho lee <optional.int@kakao.com>
2022-05-30 06:22:42 +0900

    chore: add update on reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -4,5 +4,7 @@ helm uninstall my-scale-out-test
 helm uninstall my-scale-out-load-tester-helm
 helm repo add soma-so1s-scale-out-test https://so1s.github.io/scale-out-helm
 helm repo add scale-out-load-tester-helm https://so1s.github.io/scale-out-load-tester-helm
+helm repo update soma-so1s-scale-out-test
+helm repo update scale-out-load-tester-helm
 helm install my-scale-out-test soma-so1s-scale-out-test/scale-out-test
 helm install my-scale-out-load-tester-helm scale-out-load-tester-helm/scale-out-load-tester-helm
